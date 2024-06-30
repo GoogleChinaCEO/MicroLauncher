5c83b4c9808c19693341a444362df3f30ec897ed
Yuyuko1024 <dingwenxuan4@gmail.com>
2024-06-30 19:45:01 +0800

    添加基本的ViewModel框架和SP工具类

--- app/src/main/java/org/exthmui/microlauncher/duoqin/MainApplication.java
+++ app/src/main/java/org/exthmui/microlauncher/duoqin/MainApplication.java
@@ -1,10 +1,9 @@
 package org.exthmui.microlauncher.duoqin;
 
 import android.app.Application;
-import android.content.Context;
-import android.content.SharedPreferences;
 
 import org.exthmui.microlauncher.duoqin.utils.BuglyUtils;
+import org.exthmui.microlauncher.duoqin.utils.LauncherSettingsUtils;
 
 /**
  * @author Maribel
@@ -14,8 +13,8 @@ public class MainApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        SharedPreferences sharedPreferences = getSharedPreferences(getPackageName()+"_preferences", Context.MODE_PRIVATE);
-        if (sharedPreferences.getBoolean("bugly_init",false)) {
+        if (LauncherSettingsUtils.getInstance(this)
+                .getBoolean("bugly_init",false)) {
             BuglyUtils.initBugly(this);
         }
     }
---
+++ app/src/main/java/org/exthmui/microlauncher/duoqin/model/LauncherViewModel.java
@@ -0,0 +1,7 @@
+package org.exthmui.microlauncher.duoqin.model;
+
+import androidx.lifecycle.ViewModel;
+
+public class LauncherViewModel extends ViewModel {
+
+}
